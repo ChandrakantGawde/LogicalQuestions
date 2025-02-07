44d7aedd2d0a56df798b3a5a4880525d8f978dbc
Chandrakant15 <chandrakant.gawde@finmeadows.com>
2025-02-07 18:32:12 +0530

    Swap Numbers

--- swap.js
+++ swap.js
@@ -11,9 +11,13 @@ function Swap(a, b){
 
     // Swap using 2 variables
 
-    a = a+b; // 30
-    b = a-b; // 10;
-    a = a-b; // 20
+    // a = a+b; // 30
+    // b = a-b; // 10;
+    // a = a-b; // 20
+
+    a = a*b; // 200
+    b = a/b; // 10
+    a = a/b; // 20
 
 
      console.log(`After Swap A is ${a} and B is ${b}`)
