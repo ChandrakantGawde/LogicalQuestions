79522684ab14cc108790415d3266ff3eb59fff63
Chandrakant15 <chandrakant.gawde@finmeadows.com>
2025-02-07 19:09:20 +0530

    reverseString

---
+++ reverseString.js
@@ -0,0 +1,28 @@
+var str = "chandu";
+
+function StrRev(str){
+
+    // Using charAt method
+
+    //var length = str.length; // get length
+    var getRev = '' // empty string to store result
+
+    // while(length >= 0){
+    //    var rev = str.charAt(length); // get char from end
+    //    getRev = getRev+rev; // concate
+
+    //    length--; // reduce length
+    // }
+
+
+    // convert string using charactor array split method 
+    let charArray = str.split('');
+    var length = charArray.length;
+    
+    for(let i= length-1 ; i>=0 ; i--){
+        getRev = getRev + charArray[i];
+    }
+    console.log(getRev);
+};
+
+StrRev(str);
