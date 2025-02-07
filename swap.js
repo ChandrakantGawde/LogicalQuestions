177302a03d960f55378c89b31d10a92bae286692
Chandrakant15 <chandrakant.gawde@finmeadows.com>
2025-02-07 18:27:16 +0530

    swapVariables

---
+++ swap.js
@@ -0,0 +1,22 @@
+var a = 10;
+var b = 20;
+
+function Swap(a, b){
+
+    // Swap using 3rd variable 
+    //  let temp;
+    //  temp = a;
+    //  a=b;
+    //  b= temp;
+
+    // Swap using 2 variables
+
+    a = a+b; // 30
+    b = a-b; // 10;
+    a = a-b; // 20
+
+
+     console.log(`After Swap A is ${a} and B is ${b}`)
+};
+
+Swap(a, b);
