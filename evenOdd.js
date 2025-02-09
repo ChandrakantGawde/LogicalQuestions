beb30afda0bb7fd9ecb068b206d257d016370fd1
Chandrakant15 <chandrakant.gawde@finmeadows.com>
2025-02-09 12:28:24 +0530

    large Number

--- evenOdd.js
+++ evenOdd.js
@@ -2,17 +2,18 @@
 function EvenOdd(no){
     let even = 0;
     let odd = 0;
+    let sum = 0;
     while( no > 0){
        let digit = no % 10;
-
+       sum = sum + digit;
        if(digit % 2 == 0 ){
         even++
        }else{
         odd++
        }
-
        no = Math.floor(no / 10);
     }
+    console.log("Sum is ", sum);
     console.log(`Even digits ${even} and Odd digits ${odd}`)
 }
 
---
+++ largeNumber.js
@@ -0,0 +1,10 @@
+
+function LargNumber(){
+    let a= 40;
+    let b= 50;
+    let c= 30;
+
+   return a>b ? "A is grator" : b>c ? "B is grator" : "C is grator" 
+}
+
+console.log(LargNumber())
